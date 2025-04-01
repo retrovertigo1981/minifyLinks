0239a5bff149eee07e3fabb05ece526faec1eac0
Sebastián Peña <fuzzdev@Sebastian.>
2025-03-31 22:12:01 -0300

    - [build] Instala dependencias iniciales del proyecto (Express, Sequelize, etc.) - [db] Configuración inicial de la BD (modelos, migraciones y seeders)

---
+++ config/config.js
@@ -0,0 +1,17 @@
+// Sequelize
+require('dotenv').config()
+
+const { env } = process
+
+const envConfig = {
+    "development": {
+        "username": env.DB_USER,
+        "password": env.DB_PASSWORD,
+        "database": env.DB_DATABASE,
+        "host": env.DB_HOST,
+        "dialect": "postgres",
+    },
+    JWT_SECRET: env.JWT_SECRET
+}
+
+module.exports = envConfig
